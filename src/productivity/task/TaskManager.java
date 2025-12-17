290c315c1e10d6eda8053e2b849ab007173d78e8
Elshan Iskandarli <isgndrlielsn71@gmail.com>
2025-12-17 12:26:11 +0300

    Refactor TaskManager to use TaskDAO
    Refactor TaskManager to use TaskDAO for database operations and make task and goal lists static.

--- src/productivity/task/TaskManager.java
+++ src/productivity/task/TaskManager.java
@@ -2,51 +2,62 @@ package productivity.task;
 
 import productivity.database.DatabaseConnection;
 import productivity.tracking.ProductivityTracker;
+import productivity.task.Task;
+import productivity.task.Goal;
 
 import java.sql.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    private List<Task> tasks;
-    private List<Goal> goals;
+    private static List<Task> tasks;
+    private static List<Goal> goals;
 
     public TaskManager() {
        tasks = new ArrayList<>();
        goals = new ArrayList<>();
-       loadFromDatabase(); 
+       TaskDAO.loadFromDatabase(); 
     }
 
-    public void createTask(String title, String description, LocalDateTime deadline) {
+    /**
+     * createTask() creates a new general task, saves it to the database, and adds it to the databbase.
+     */
+    public static void createTask(String title, String description, LocalDateTime deadline) {
         Task task = new Task(title, description, deadline);
-        saveTaskToDatabase(task);
+        TaskDAO.saveTaskToDatabase(task);
         tasks.add(task);
         System.out.println(title+ "has been added to tasks.");
     }
 
-    public void createGoal(String title, String description, LocalDateTime targetDate,boolean isLongTerm, String category) {
-        Goal goal = new Goal(title, description, targetDate, isLongTerm, category);
-        saveGoalToDatabase(goal);
+    /**
+     * createGoal() creates a new goal
+     */
+    public void createGoal(int id, String title, String description, LocalDateTime targetDate, boolean isLongTerm, String category) {
+        Goal goal = new Goal(id, title, description, targetDate, isLongTerm, category);
+        TaskDAO.saveGoalToDatabase(goal);
         goals.add(goal);
         System.out.println(title + "has been added to goals.");
     }
 
+    /**
+     * This method marks the task with given id as complete and rewards user with XP
+     */
     public void completeTask(int id) {
         for(Task task : tasks) {
             if(task.getId() == id) {
                 task.markAsComplete();
-                updateTaskInDatabase(task);
+                TaskDAO.updateTaskInDatabase(task);
                 ProductivityTracker.addXP(task.getXpReward());
                 return;
             }
         }
-        System.out.println("No such task can be found.");
+        System.out.println("No such task found.");
     }
 
+    /**
+     * This method returns tasks that are overdue
+     */
     public List<Task> getOverdueTasks() {
         List<Task> overdues = new ArrayList<>();
         for(Task task : tasks) {
@@ -58,28 +69,20 @@ public class TaskManager {
         return overdues;
     }
 
+    /**
+     * This method returns all the incomplete tasks that must be completed today
+     */
     public List<Task> todaysTasks() {
         List<Task> forToday = new ArrayList<>();
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime tomorrow = now.plusDays(1).withHour(0).withMinute(0);
 
         for(Task task : tasks) {
-            if(!task.isComplete() && !task.isOverdue() && task.getDeadline().isAfter(now) 
-                && task.getDeadline().isBefore(tomorrow)) {
-                    forToday.add(task);
+            if(!task.isComplete() && !task.isOverdue() && task.getDeadline().isAfter(now) && task.getDeadline().isBefore(tomorrow)) {
+                forToday.add(task);
             }
         }
 
         return forToday;
     }
-
-    private void saveTaskToDatabase(Task task) {
-        
-    }
-
-     private void saveGoalToDatabase(Goal goal) {
-
-        saveTaskToDatabase(goal);
-    }
-
 }
