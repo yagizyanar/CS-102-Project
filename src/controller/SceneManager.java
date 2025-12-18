0f7003c82b68903d5ff58e8caefb865ba475ca5f
Muhammed Serhat Mutlu <muhammedserhatmutlu@gmail.com>
2025-12-18 17:39:27 +0300

    Classes are arranged as model and dao classes

---
+++ src/controller/SceneManager.java
@@ -0,0 +1,25 @@
+package project;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+
+    private static Stage primaryStage;
+
+    public static void setStage(Stage stage) {
+        primaryStage = stage;
+    }
+
+    public static void switchScene(String fxmlPath) {
+        try {
+            Parent root = FXMLLoader.load(SceneManager.class.getResource(fxmlPath));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/productivity/timer/tst.java
+++
@@ -1,5 +0,0 @@
-public class tst {
-    private static void main(String[] args) {
-        System.out.println("");
-    }
-}
